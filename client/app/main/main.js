cbb77b7d299f2824f545c8390c0db35370916fc9
Nikola Novakovic <nikola.novakovic9@gmail.com>
2015-07-30 15:08:02 +0200

    Init commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('chatExpressApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
